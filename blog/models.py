9008911926b459a2c7b13a47bf6be5b3945411c7
hojjat faryabi <hojjat.faryabi@gmail.com>
2020-11-17 18:45:48 +0330

    add models and home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField 
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=False, blank=False, default="")
+    content = HTMLField(null=False, blank=False)
+    category = models.ForeignKey(Category, null=False, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
